80bf63b6029db6652cc2dcbb3ca7fb588e19d529
Win Lwin <114051612+winlwinIosdeveloper@users.noreply.github.com>
2023-08-04 21:51:46 +0630

    making calculator responsive

--- cal.js
+++ cal.js
@@ -1,15 +1,29 @@
-function getNumber(input) {
-    let value = document.getElementById('textbox').value;
-    let number = input.innerText;
-
-    if (value.length == 1 && value == "0" )
-    {
-        return document.getElementById('textbox').value = number;
-    }
-    return document.getElementById('textbox').value = value + number;
-    
-    
-}
+// const display = document.getElementById('textbox');
+// const buttons = document.querySelectorAll("button");
 
 
+// const specialCharacter = ["+", "-", "*", "/", "="];
 
+// let output = "";
+
+// const calculate = (btnValue) =>  {
+//     if (btnValue === "=" && output !== "") {
+//         // output = eval(output);
+//         console.log(typeof(output));
+//     }
+//     else if (btnValue === "AC") {
+//         output = "";
+//     } else {
+//         if (output === "" && specialCharacter.includes(btnValue)) {
+//             return;
+//         }
+//         output += btnValue;
+//         console.log(output);
+//     }
+//     display.value = output;
+
+// };
+
+// buttons.forEach( (button) => {
+//     button.addEventListener('click', () => calculate(button.textContent)); 
+// })
